eb0ef2292b5bef0f984298d5ff36ad3971508b34
TheHGuito1602 <hguito1602@gmail.com>
2024-02-08 23:23:04 -0600

    Protected implementado a tareas controller

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler')
 const Tarea = require('../models/tareasModel')
 
 const getTareas = asyncHandler( async(req,res) =>{
-    const tareas =await Tarea.find();
+    const tareas =await Tarea.find({user: req.user.id});
     res.status(200).json(tareas)
 })
 
@@ -12,7 +12,8 @@ const createTareas = asyncHandler( async (req,res) =>{
         throw new Error('Por favor escriba una description');
     }
     const tarea= await Tarea.create({
-        descripcion: req.body.descripcion
+        descripcion: req.body.descripcion,
+        user: req.user.id
     })
     res.status(201).json(tarea)
 })
@@ -25,8 +26,13 @@ const updateTareas = asyncHandler( async(req,res) =>{
         throw new Error('Esa tarea no existe')
     }
     
-    const tareaUpdate = await Tarea.findByIdAndUpdate(req.params.id, req.body,{new:true})
-    res.status(200).json(tareaUpdate)
+    if(tarea.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('Usuario no autorizado')
+    }else{
+        const tareaUpdate = await Tarea.findByIdAndUpdate(req.params.id, req.body,{new:true})
+        res.status(200).json(tareaUpdate)
+    }
 })
 
 const deleteTareas = asyncHandler( async(req,res) =>{
@@ -35,9 +41,14 @@ const deleteTareas = asyncHandler( async(req,res) =>{
         res.status(400)
         throw new Error('Esa tarea no existe')
     }
-    await Tarea.deleteOne(tarea)
-    //const tareaDeleted = await Tarea.findByIdAndDelete(req.params.id)
-    res.status(200).json({id: req.params.id})
+    if(tarea.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('Usuario no autorizado')
+    }else{
+        await Tarea.deleteOne(tarea)
+        //const tareaDeleted = await Tarea.findByIdAndDelete(req.params.id)
+        res.status(200).json({id: req.params.id})
+    }
 })
 
 module.exports={
--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {getTareas, createTareas, updateTareas, deleteTareas} = require('../controllers/tareasControllers');
+const {protect} = require('../middleware/authMiddleWare');
 
-//router.route('/').get(getTareas).post(createTareas)
+//router.route('/').get(protect, getTareas).post(createTareas)
 
-router.get('/', getTareas);
-router.post('/', createTareas);
+router.get('/', protect, getTareas);
+router.post('/', protect, createTareas);
 
 //router.route('/:id').delete(deleteTareas).put(updateTareas)
-router.put('/:id', updateTareas);
-router.delete('/:id', deleteTareas);
+router.put('/:id', protect,updateTareas);
+router.delete('/:id', protect,deleteTareas);
 
 module.exports = router
