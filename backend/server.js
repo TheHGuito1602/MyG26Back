93c289f25141536d85b28d3c7048650513191b21
TheHGuito1602 <hguito1602@gmail.com>
2024-02-08 23:45:33 -0600

    Cors implementado

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleWare')
 const connectDB = require('./config/db')
 const port = process.env.PORT||5000;
+const cors = require('cors')
 
 connectDB();
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
